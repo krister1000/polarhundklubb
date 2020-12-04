abcdfd16b0af8480ae9c486e130fada34c224010
Krister Hunnes <kristerhunnes@gmail.com>
2020-12-04 19:05:05 +0100

    update all blocks

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,15 @@
-const { Collapse } = require("bootstrap");
 
-$menuButton = document.querySelector('.menu__icon');
 
-console.log($menuButton);
+const menuButtonOpen = document.querySelector('.menu__icon');
+const menuButtonClose = document.querySelector('.menu__icon--close');
+const headerToggle = document.querySelector('.menu__overlay');
+
+menuButtonOpen.addEventListener('click', openMenu);
+menuButtonClose.addEventListener('click', closeMenu);
+
+function openMenu() {
+    headerToggle.classList.toggle('menu__overlay--active');
+}
+function closeMenu() {
+    headerToggle.classList.toggle('menu__overlay--active');
+}
